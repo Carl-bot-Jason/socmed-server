fc5896228e7f54060e06af99277e17c978d96a0c
Carl Jason <73461353+Carl-bot-Jason@users.noreply.github.com>
2023-05-01 22:33:36 +0530

    Handled Database connection failure

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require('express');
-const router = require('./router');
 const cors = require('cors');
 const connect_db  = require('./db/connect');
 const errorMiddleware = require('./errors/errorMiddleware');
-const socketServer = require('./socket');
 require('dotenv').config();
 
-const app = express();
+connect_db()
+.then(con => {
+	const router = require('./router');
+	const socketServer = require('./socket');
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/api', router);
-app.use(errorMiddleware);
-
-const port = process.env.PORT || 3005;
-
-async function start(){
-	let con = await connect_db();
+	const app = express();
+	
+	app.use(cors());
+	app.use(express.json());
+	app.use(express.urlencoded({extended: true}));
+	app.use('/api', router);
+	app.use(errorMiddleware);
+	
+	const port = process.env.PORT || 3005;
+	
 	app.listen(port, () => {
 		console.log(`Server is listening on port ${port}...`);
 	});
 	
 	socketServer.listen(4000);
-}
-
-start();
+})
+.catch(err => {
+	console.log('Database connection failed');
+});
