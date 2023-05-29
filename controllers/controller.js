fffa3c0aac9b71c1d78c74b90eb5d02ca9aefe38
Carl Jason <73461353+Carl-bot-Jason@users.noreply.github.com>
2023-05-29 13:59:26 +0530

    testing

--- controllers/controller.js
+++ controllers/controller.js
@@ -45,7 +45,7 @@ async function postLogin(req, res, next){
 		}
 		let jwt = await createJWT(req.body.username);
 		res.clearCookie('token')
-		.cookie('token', jwt, {expires: new Date(Date.now() + 60 * 1000 * 86400)})
+		.cookie('token', jwt, {expires: new Date(Date.now() + 60 * 1000 * 86400), Secure: true})
 		.status(200)
 		.send({response: "Login successful"});
 	}
