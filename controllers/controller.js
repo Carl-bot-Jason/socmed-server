bb47f4e20e4bc9f88132292d986c207a49462684
Carl Jason <73461353+Carl-bot-Jason@users.noreply.github.com>
2023-08-13 16:51:12 +0530

    Testing

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,20 +4,11 @@ require('dotenv').config();
 
 async function getUsername(req, res, next){
 	try{
-		console.log(req);
-		console.log('--------------------');
-		console.log(req.headers);
-		console.log('--------------')
-		console.log(req.headers.cookie);
-		if(!req.headers.cookie){
-			res.status(409).send({error: 'Invalid cookies'});
+		if(!req.headers.cookie || !/token=([^;]+)/.exec(req.headers.cookie)){
+			res.status(401).send({error: 'Invalid cookies'});
 			return;
 		}	
-		let token = /token=(.+)/.exec(req.headers.cookie)[1];
-		if(!token){
-			res.status(409).send({error: 'Invalid cookies'});
-			return;
-		}
+		let token = /token=([^;]+)/.exec(req.headers.cookie)[1];
 		let decoded = await verifyJWT(token);
 		res = setHeaders(res);
 		if(decoded.payload){
